7a413e51ccf98504943b1a5668855705fb0859b5
yaroslav <bus.yaroslav@gmail.com>
2016-12-22 14:37:13 +0300

    oop

---
+++ src/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import Display.Display;
+
+public class Main {
+	public static void main(String[] args){
+		Display.create(800, 600, "Game", 0xff00ff00);
+		Timer t= new Timer(1000 / 60, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Display.clear();
+				Display.render();
+				Display.swapBuffers();
+			}
+		});
+		t.setRepeats(true);
+		t.start();
+	}
+}
